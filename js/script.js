f6705cd3701bacad09b1b54f4cc565af18db1e53
Judy Bonney <judylbonney@gmail.com>
2023-12-05 14:31:52 -0500

    Added a list of repos to the website

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // Profile information will appear here
 const overview= document.querySelector(".overview");
-const username = 'judy-b'
+const username = 'judy-b';
+// unordered list to display the repos list
+const repoList = document.querySelector(".repo-list");
+
 const getGitHubData= async function() {
     const response = await fetch(`https://api.github.com/users/${username}`);
     const data = await response.json();
@@ -12,6 +15,22 @@ const displayData = async function(data) {
     div.classList.add("user-info");
     div.innerHTML = `<figure><img alt="user avatar" src=${data.avatar_url} /></figure><div><p><strong>Name:</strong> ${data.name}</p><p><strong>Bio:</strong> ${data.bio}</p><p><strong>Location:</strong> ${data.location}</p><p><strong>Number of public repos:</strong> ${data.public_repos}</p></div>`;
     overview.append(div);
+    fetchRepos();
+}
+getGitHubData()
+
+const fetchRepos = async function() {
+    const data = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repos = await data.json();
+    console.log(repos);
+    displayRepos(repos);
 }
 
-getGitHubData()
+const displayRepos = function(repos) {
+    for (let repo of repos) {
+    const li = document.createElement("li");
+    li.classList.add("repo");
+    li.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(li);
+    }
+}
